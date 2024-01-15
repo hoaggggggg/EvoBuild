b86c7d50a610c2202860256260634ef069578250
hoaggggggg <156327794+hoaggggggg@users.noreply.github.com>
2024-01-15 22:22:13 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf libxml2 lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev imagemagick git lunzip lzop schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python3 perl xmlstarlet virtualenv xz-utils rr jq libncurses5 pngcrush lib32ncurses5-dev git-lfs libxml2 openjdk-11-jdk-headless repo
